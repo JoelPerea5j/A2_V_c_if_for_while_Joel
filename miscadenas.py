7714ad2d404516e429a2a60f4935cfe308da4b06
JoelPerea5j <a.22308051281272@cbtis128.edu.mx>
2024-09-06 09:33:07 -0600

    Add files via upload
    Subí 2 Archivos

---
+++ miscadenas.py
@@ -0,0 +1,25 @@
+'''Joel Alberto
+Trabajando con Cadenas
+'''
+
+nombre="Joel Alberto Perea Castorena"
+print(nombre[1])
+print(nombre)
+
+Misnovias=["si","sifq","xnwo"]
+for j in Misnovias:
+  print(j)
+
+  print("otro ejemplo")
+b = "Joel_Perea"
+print(b[2:5])
+
+print(" a mayusculas")
+a = "joel Perea"
+print(a.upper())
+
+print("Concatenar")
+a = "karen "
+b = "zy"
+c = a +"_"+ b
+print(c)
